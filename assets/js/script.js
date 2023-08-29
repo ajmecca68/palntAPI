6edefa52179727d51a6a6b298ec638bbab452e02
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2023-08-29 10:21:39 -0400

    created a working prototype of API

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,45 @@
-const apiKey = 'LRnoSZ98nrCPuGd81qZ0Bez69FQxN4-ur3OgZReMxOk';
-const searchButton = document.getElementById('searchButton');
-const plantNameInput = document.getElementById('plantName');
-const plantInfoDiv = document.getElementById('plantInfo');
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.getElementById("searchButton");
+    const plantNameInput = document.getElementById("plantName");
+    const plantInfoDiv = document.getElementById("plantInfo");
 
-searchButton.addEventListener('click', async () => {
-    const plantName = plantNameInput.value;
-    if (plantName) {
-        await getPlantInfo(plantName);
-    }
+    searchButton.addEventListener("click", () => {
+        const plantName = plantNameInput.value;
+
+        if (plantName) {
+            const apiKey = "sk-iTS464edf444c5e1f2019";
+            const apiUrl = `https://perenual.com/api/species-list?key=${apiKey}&q=${encodeURIComponent(plantName)}`;
+
+            fetch(apiUrl)
+            .then(response => response.json())
+            .then(data => {
+                const plantData = data.data;
+
+                if (plantData.length > 0) {
+                    const plantInfo = plantData.map(plant => 
+           
+                        `    
+                        <div>
+                            <h2>${plant.common_name}</h2>
+                            <p>Scientific Name: ${plant.scientific_name.join(", ")}</p>
+                            <p>Watering: ${plant.watering}</p>
+                            <p>Cycle: ${plant.cycle}</p>
+                        </div>
+                    `).join("");
+
+                    plantInfoDiv.innerHTML = plantInfo;
+                } else {
+                    plantInfoDiv.innerHTML = "<p>No plants found with that name.</p>";
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching plant information:", error);
+                plantInfoDiv.innerHTML = "<p>An error occurred while fetching plant information.</p>";
+            });
+        } else {
+            plantInfoDiv.innerHTML = "<p>Please enter a plant name.</p>";
+        }
+    });
 });
 
-async function getPlantInfo(plantName) {
-    const apiUrl = `https://trefle.io/api/v1/plants/search?token=${apiKey}&q=${plantName}`;
 
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-
-        if (data.data && data.data.length > 0) {
-            const plant = data.data[0];
-            const commonName = plant.common_name || 'N/A';
-            const family = plant.family_common_name || 'N/A';
-            const scientificName = plant.scientific_name || 'N/A';
-
-            plantInfoDiv.innerHTML = `
-                <h2>${commonName}</h2>
-                <p><strong>Scientific Name:</strong> ${scientificName}</p>
-                <p><strong>Family:</strong> ${family}</p>
-            `;
-        } else {
-            plantInfoDiv.innerHTML = '<p>No plant found with that name.</p>';
-        }
-    } catch (error) {
-        console.error('Error fetching plant information:', error);
-        plantInfoDiv.innerHTML = '<p>An error occurred while fetching plant information.</p>';
-    }
-}
