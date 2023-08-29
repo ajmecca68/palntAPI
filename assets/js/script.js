84a56bf1bff3dd3f56e0233c43a028dc4608d9dc
ajmecca68 <137560371+ajmecca68@users.noreply.github.com>
2023-08-29 11:55:40 -0400

    added a test search for trefle and an image of the plant

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
     const searchButton = document.getElementById("searchButton");
     const plantNameInput = document.getElementById("plantName");
-    const plantInfoDiv = document.getElementById("plantInfo");
+    const perunialInfoDiv = document.getElementById("perunialInfo");
 
     searchButton.addEventListener("click", () => {
         const plantName = plantNameInput.value;
 
         if (plantName) {
-            const apiKey = "sk-iTS464edf444c5e1f2019";
-            const apiUrl = `https://perenual.com/api/species-list?key=${apiKey}&q=${encodeURIComponent(plantName)}`;
-
-            fetch(apiUrl)
+            const perunialKey = "sk-iTS464edf444c5e1f2019";
+            const perunialUrl = `https://perenual.com/api/species-list?key=${perunialKey}&q=${encodeURIComponent(plantName)}`;
+            fetch(perunialUrl)
             .then(response => response.json())
             .then(data => {
-                const plantData = data.data;
-
+                const plantData = data.data; 
                 if (plantData.length > 0) {
-                    const plantInfo = plantData.map(plant => 
-           
-                        `    
-                        <div>
-                            <h2>${plant.common_name}</h2>
-                            <p>Scientific Name: ${plant.scientific_name.join(", ")}</p>
-                            <p>Watering: ${plant.watering}</p>
-                            <p>Cycle: ${plant.cycle}</p>
-                        </div>
-                    `).join("");
+                    const firstPlant = plantData[0]; // Get the first plant
 
-                    plantInfoDiv.innerHTML = plantInfo;
+                    const perunialInfo = `
+                        <div>
+                            <h2>${firstPlant.common_name}</h2>
+                            <p>Scientific Name: ${firstPlant.scientific_name.join(", ")}</p>
+                            <p>Watering: ${firstPlant.watering}</p>
+                            <p>Cycle: ${firstPlant.cycle}</p>
+                            <img src="${firstPlant.default_image.small_url}" alt="${firstPlant.common_name} Image">
+                        </div>
+                    `;
+
+                    perunialInfoDiv.innerHTML = perunialInfo;
                 } else {
-                    plantInfoDiv.innerHTML = "<p>No plants found with that name.</p>";
+                    perunialInfoDiv.innerHTML = "<p>No plants found with that name.</p>";
                 }
             })
             .catch(error => {
                 console.error("Error fetching plant information:", error);
-                plantInfoDiv.innerHTML = "<p>An error occurred while fetching plant information.</p>";
+                perunialInfoDiv.innerHTML = "<p>An error occurred while fetching plant information.</p>";
             });
         } else {
-            plantInfoDiv.innerHTML = "<p>Please enter a plant name.</p>";
+            perunialInfoDiv.innerHTML = "<p>Please enter a plant name.</p>";
         }
     });
 });
-
-
---
+++ assets/js/trefle.js
@@ -0,0 +1,44 @@
+// trefle.js
+
+document.addEventListener("DOMContentLoaded", () => {
+    const searchButton = document.getElementById("searchButton");
+    const plantNameInput = document.getElementById("plantName");
+    const trefleInfoDiv = document.getElementById("trefleInfo");
+
+    searchButton.addEventListener("click", () => {
+        const plantName = plantNameInput.value;
+
+        if (plantName) {
+            // Trefle API call
+            const trefleToken = "LRnoSZ98nrCPuGd81qZ0Bez69FQxN4-ur3OgZReMxOk";
+            const trefleUrl = `https://trefle.io/api/v1/plants?token=${trefleToken}&filter[common_name]=${encodeURIComponent(plantName)}`;
+            fetch(trefleUrl)
+            .then(response => response.json())
+            .then(data => {
+                const plantData = data.data; 
+                if (plantData.length > 0) {
+                    const trefleInfo = plantData.map(plant => 
+                        `
+                        <div>
+                            <h2>${plant.scientific_name}</h2>
+                            <p>Trefle.io</p>
+                            <p>Family: ${plant.family}</p>
+                            <p>Genus: ${plant.genus}</p>
+                            <p>Author: ${plant.author}</p>
+                        </div>
+                    `).join("");
+
+                    trefleInfoDiv.innerHTML = trefleInfo;
+                } else {
+                    trefleInfoDiv.innerHTML = "<p>No plants found with that name.</p>";
+                }
+            })
+            .catch(error => {
+                console.error("Error fetching plant information from Trefle:", error);
+                trefleInfoDiv.innerHTML = "<p>An error occurred while fetching plant information from Trefle.</p>";
+            });
+        } else {
+            trefleInfoDiv.innerHTML = "";
+        }
+    });
+});
